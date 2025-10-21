064b1abded85561dfb4fdc566fbe67da424e7811
CristianLandri <patobukclee@gmail.com>
2025-10-22 00:00:20 +0200

    Add files via upload

---
+++ Carussel.js
@@ -0,0 +1,14 @@
+var swiperProjects = new Swiper (".myswiper",{
+  effect: "coverflex",
+  grabcursor: true,
+  centeredslides: true,
+  slidesperview: "auto",
+  coverfloweffect: {
+    rotate:15,
+    strech:0,
+    depth:300,
+    modifier:1,
+    slideshadows:true,
+  },
+    loop:true,
+});
