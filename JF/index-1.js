ed7c6fb3e575f5bcdde5c5a0f03f09545dcecd7e
CristianLandri <patobukclee@gmail.com>
2025-11-14 00:45:52 -0300

    Add files via upload

--- JF/index-1.js
+++ JF/index-1.js
@@ -1,9 +1,11 @@
-const btnCart = document.querySelector('.container-icon')
-const containerCartProducts = document.querySelector('.container-cart-products')
+const btnCart = document.querySelector('.container-icon');
+const containerCartProducts = document.querySelector('.container-cart-products');
 
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart')
-})
+if (btnCart && containerCartProducts) {
+	btnCart.addEventListener('click', () => {
+		containerCartProducts.classList.toggle('hidden-cart');
+	});
+}
 /* ========================= */
 const cartInfo = document.querySelector('.cart-product');
 const rowProduct = document.querySelector('.row-product');
@@ -21,52 +23,58 @@ const countProducts = document.querySelector('#contador-productos');
 const cartEmpty = document.querySelector('.cart-empty');
 const cartTotal = document.querySelector('.cart-total');
 
-productsList.addEventListener('click', e => {
-	if (e.target.classList.contains('btn-add-cart')) {
-		const product = e.target.parentElement;
 
-		const infoProduct = {
-			quantity: 1,
-			title: product.querySelector('h2').textContent,
-			price: product.querySelector('p').textContent,
-		};
+if (productsList) {
+	productsList.addEventListener('click', e => {
+		if (e.target.classList.contains('btn-add-cart')) {
+			const product = e.target.parentElement;
 
-		const exits = allProducts.some(
-			product => product.title === infoProduct.title
-		);
+			const infoProduct = {
+				quantity: 1,
+				title: product.querySelector('h2').textContent,
+				price: product.querySelector('p').textContent,
+			};
 
-		if (exits) {
-			const products = allProducts.map(product => {
-				if (product.title === infoProduct.title) {
-					product.quantity++;
-					return product;
-				} else {
-					return product;
-				}
-			});
-			allProducts = [...products];
-		} else {
-			allProducts = [...allProducts, infoProduct];
+			const exits = allProducts.some(
+				product => product.title === infoProduct.title
+			);
+
+			if (exits) {
+				const products = allProducts.map(product => {
+					if (product.title === infoProduct.title) {
+						product.quantity++;
+						return product;
+					} else {
+						return product;
+					}
+				});
+				allProducts = [...products];
+			} else {
+				allProducts = [...allProducts, infoProduct];
+			}
+
+			showHTML();
 		}
+	});
+}
 
-		showHTML();
-	}
-});
 
-rowProduct.addEventListener('click', e => {
-	if (e.target.classList.contains('icon-close')) {
-		const product = e.target.parentElement;
-		const title = product.querySelector('p').textContent;
+if (rowProduct) {
+	rowProduct.addEventListener('click', e => {
+		if (e.target.classList.contains('icon-close')) {
+			const product = e.target.parentElement;
+			const title = product.querySelector('p').textContent;
 
-		allProducts = allProducts.filter(
-			product => product.title !== title
-		);
+			allProducts = allProducts.filter(
+				product => product.title !== title
+			);
 
-		console.log(allProducts);
+			console.log(allProducts);
 
-		showHTML();
-	}
-});
+			showHTML();
+		}
+	});
+}
 
 // Funcion para mostrar  HTML
 const showHTML = () => {
